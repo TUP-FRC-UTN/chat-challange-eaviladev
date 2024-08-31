7087115340500e8b5f2e0c35e4e64e03f6d0a2b3
eaviladev <esteban.avila.dev@gmail.com>
2024-08-30 21:02:09 -0300

    muevo los chat al componente sala

--- chatroom/src/app/sala/sala.component.ts
+++ chatroom/src/app/sala/sala.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MensajeUsuarioComponent } from '../mensaje-usuario/mensaje-usuario.component';
 
 @Component({
   selector: 'app-sala',
   standalone: true,
-  imports: [],
+  imports: [MensajeUsuarioComponent],
   templateUrl: './sala.component.html',
   styleUrl: './sala.component.css'
 })
 export class SalaComponent {
-
+  manejarMensaje(evento: { usuario: string, msj: string }) {
+    console.log('Mensaje recibido del usuario:', evento.usuario);
+    console.log('Contenido del mensaje:', evento.msj);
+  }
 }
